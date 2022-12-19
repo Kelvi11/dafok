37b4a279c97a1ca7cc4e14118d4f70f810ed9b7f
dervishllari <kelvin.dervishllari@moveoalbania.com>
2022-12-19 12:37:00 +0100

    structure

---
+++ src/main/java/com/kelvin/dafok/DafokApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.dafok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DafokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DafokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelvin/dafok/DafokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.dafok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DafokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
